3bd60a1e8a571a5a6f6ee2eaf832988f3e0ceec1
sunyoung9797 <allena4302@naver.com>
2022-12-03 19:16:22 +0900

    init

---
+++ src/test/java/com/twitter/twt/TwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.twt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
